d5a6cee8caa877f57786a15aee5d604a1c2d55d1
Rahul Mamoria <rmamoria@griddynamics.com>
2024-07-17 12:20:34 +0530

    learning angular routing

--- Angular forms/Reactive/src/app/auth/login/login.component.ts
+++ Angular forms/Reactive/src/app/auth/login/login.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule],
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css"
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl("", {
+      validators:[Validators.email, Validators.required],
+
+    }),
+    password: new FormControl("", {
+      validators:[Validators.required, Validators.minLength(6)]
+    })
+  });
+
+  get emailIsInvalid(){
+    return this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid
+  }
+  get passwordIsInvalid(){
+    return this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid
+  }
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.email;
+  }
+}
